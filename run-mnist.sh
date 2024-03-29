3c4db8693b4c62f20f4008f378c383900a6da64d
manote101 <manote.usa@gmail.com>
2024-03-29 13:44:16 +0700

    Add files via upload
    first commit

---
+++ run-mnist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Clone Demo MNIST trainig in PyTorch
+git clone https://github.com/ChawDoe/LeNet5-MNIST-PyTorch.git
+
+# copy training program
+cp mnist-train1.py LeNet5-MNIST-PyTorch
+
+# copy Slurm script
+cp sing-normal.sub LeNet5-MNIST-PyTorch
+
+cd LeNet5-MNIST-PyTorch
+sbatch sing-normal.sub
